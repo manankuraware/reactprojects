f618740e08fdaa7289a8b875be8a92538be256b6
Manan Kuraware <manankuraware2017@gmail.com>
2025-08-17 22:51:54 +0530

    navabar

--- portfolio_site/src/component/Header.jsx
+++ portfolio_site/src/component/Header.jsx
@@ -1,26 +1,38 @@
 import React from "react";
 import styles from "./Header.module.css";
+import profilePic from "../assets/ai.jpg";
 
 export default function Header() {
   return (
     <header className="headersection">
       <nav className="header-container page-width">
-        <div className="header-wrapper">
+        <div className={styles["header-wrapper"]}>
           <div className="header-col1">
-            <a href="">
-              <div className="header-icon">
-                <img src="" alt="" />
-              </div>
+            <a href="/" className={styles.headerIcons}>
+              <img
+                src={profilePic}
+                alt="profilePic"
+                width="50"
+                height="50"
+                style={{ borderRadius: "50%" }}
+              />
               <span className="textlogo">Manan</span>
             </a>
           </div>
-          <div className="header-col2">
-            <ul className="navlist">
-              <li className="navitems">item 1</li>
-              <li className="navitems">item 2</li>
-              <li className="navitems">item 3</li>
-              <li className="navitems">item 4</li>
+          <div className={styles["header-col2"]}>
+            <ul className={styles.navlist}>
+              <li className={styles.navitems}>Home</li>
+              <li className={styles.navitems}>features</li>
+              <li className={styles.navitems}>Portfolio</li>
+              <li className={styles.navitems}>resume</li>
+              <li className={styles.navitems}>blog</li>
+              <li className={styles.navitems}>contact</li>
             </ul>
+            <div className="header-button">
+              <a href="#" className="btn">
+                Hire now
+              </a>
+            </div>
           </div>
         </div>
       </nav>
