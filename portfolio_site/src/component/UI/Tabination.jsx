5e6d1db274ce9b41343b282c11366d2e27c6a3f1
Manan Kuraware <manankuraware2017@gmail.com>
2025-08-21 23:33:05 +0530

    tabination content

--- portfolio_site/src/component/UI/Tabination.jsx
+++ portfolio_site/src/component/UI/Tabination.jsx
@@ -1,9 +1,10 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { Profile } from "../UI/Profile";
 import { Interest } from "../UI/Interest";
 import { Settings } from "../UI/Settings";
 
 export const Tabination = () => {
+  const [activeTab, setActiveTab] = useState(0);
   const tabs = [
     {
       name: "Profile",
@@ -18,13 +19,18 @@ export const Tabination = () => {
       component: Settings,
     },
   ];
-
+  const ActiveTabComponent = tabs[activeTab].component;
   return (
     <div className="page-width">
       <div className="tab-heading-container">
-        {tabs.map((t) => (
-          <div className="tab-heading">{t.name}</div>
+        {tabs.map((t, index) => (
+          <div className="tab-heading" onClick={() => setActiveTab(index)}>
+            {t.name}
+          </div>
         ))}
+      </div>
+      <div className="tab-content">
+        <ActiveTabComponent />
       </div>
     </div>
   );
