21930eb6c9bcce672f4b54806cb80df3579204d5
Manan Kuraware <manankuraware2017@gmail.com>
2025-09-01 23:43:41 +0530

    interface

--- TypeScript/app.js
+++ TypeScript/app.js
@@ -1,5 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var a = 10;
-console.log(a);
+let a = 10;
+a = "Manan";
+let b = 20;
+// b = "manan2" error
+let arr = [1, 2, 3, { name: "manan" }, { name: 12 }];
+typeof (arr);
+// tuples: having fix size and fix datatype for indexs eg: 
+const arr2 = ["Mnanan", 33];
+// enumeration 
+var UserRoles;
+(function (UserRoles) {
+    UserRoles["ADMIN"] = "admin";
+    UserRoles["GUEST"] = "guest";
+    UserRoles["SUPER_ADMIN"] = "super admin";
+})(UserRoles || (UserRoles = {}));
+UserRoles.SUPER_ADMIN;
+// unkown type: when constion run on type 
+let a2;
+a2 = 12;
+a2 = "Manan2";
+if (typeof a2 === "string")
+    a2.toLocaleUpperCase();
 //# sourceMappingURL=app.js.map
--- TypeScript/app.ts
+++ TypeScript/app.ts
@@ -1,3 +1,56 @@
-var a = 10;
+let a: any = 10;
+a = "Manan"
 
-console.log(a);
+let b: number = 20;
+// b = "manan2" error
+
+let arr = [1, 2, 3, { name: "manan" }, { name: 12 }]
+typeof (arr)
+
+// tuples: having fix size and fix datatype for indexs eg: 
+const arr2: [string, number] = ["Mnanan", 33];
+
+// enumeration 
+enum UserRoles {
+    ADMIN = "admin",
+    GUEST = "guest",
+    SUPER_ADMIN = "super admin"
+}
+
+UserRoles.SUPER_ADMIN
+
+
+// unkown type: when constion run on type 
+let a2: unknown;
+
+a2 = 12;
+a2 = "Manan2"
+
+if (typeof a2 === "string") a2.toLocaleUpperCase()
+
+// interface
+interface User {
+    name: string,
+    email: string,
+    password: string,
+    gender?: string
+}
+// here we defined stucture of object using interface if we create object of this structure we need to defined this structue except with ? its optional
+function abcd(obj: User) { }
+abcd({ name: "Manan", email: "sxyboy@gmail.com", password: "fu", gender: "male" })
+
+interface Admin extends User {
+    admin: boolean
+}
+
+// inhertance in interface 
+
+function interf(obj: Admin) {
+    obj.admin = false;
+}
+interf({ name: "Manan", email: "sxyboy@gmail.com", password: "fu", gender: "male", admin: true });
+
+
+// type alias : defining custom type 
+type val = string | number | null;
+let b2: val;
