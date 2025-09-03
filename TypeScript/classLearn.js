dcc47b4768cd8700765c886eb353846bfef09c89
Manan Kuraware <manankuraware2017@gmail.com>
2025-09-03 23:36:37 +0530

    object

--- TypeScript/classLearn.js
+++ TypeScript/classLearn.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Device = /** @class */ (function () {
     function Device() {
         this.name = "Mk";
@@ -16,3 +31,24 @@ var BottleMaker = /** @class */ (function () {
     return BottleMaker;
 }());
 var b1 = new BottleMaker("Million", 1200);
+// protected is same as private but we can acces protected variable and method inside inherited class
+var HomeMaker = /** @class */ (function () {
+    function HomeMaker() {
+        this.name = "Milton";
+    }
+    return HomeMaker;
+}());
+var KitchenHomeMaker = /** @class */ (function (_super) {
+    __extends(KitchenHomeMaker, _super);
+    function KitchenHomeMaker() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.appliance = "Gas";
+        return _this;
+    }
+    KitchenHomeMaker.prototype.changeName = function () {
+        this.name = "Some Other Name";
+    };
+    return KitchenHomeMaker;
+}(HomeMaker));
+var dw = new KitchenHomeMaker();
+dw.changeName();
--- TypeScript/classLearn.ts
+++ TypeScript/classLearn.ts
@@ -8,6 +8,25 @@ let d1 = new Device();
 let d2 = new Device();
 
 class BottleMaker {
-    constructor(public name: string, public price: number) { }
+    constructor(public name: string, public readonly price: number) { }
+    changePrice() {
+        // this.price=90   /cant chanege readonly values
+    }
 }
 const b1 = new BottleMaker("Million", 1200);
+
+
+// protected is same as private but we can acces protected variable and method inside inherited class
+class HomeMaker {
+    protected name = "Milton";
+}
+class KitchenHomeMaker extends HomeMaker {
+    public appliance = "Gas";
+    changeName() {
+        this.name = "Some Other Name";
+    }
+}
+
+let dw = new KitchenHomeMaker();
+dw.changeName();
+
