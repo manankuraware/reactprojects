e61df1b7510a90f399d49f885f171f29e254b089
Manan Kuraware <manankuraware2017@gmail.com>
2025-09-19 07:27:49 +0530

    Mern stack Food Delivery

---
+++ Food Delivery Mern Stack/backend/models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import { use } from "react";
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String
+    },
+    mobile: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ["user", "owner", "deliveryBoy"],
+        required: true
+    }
+}, { timestamps: true })
+
+// added timestamp to see when data is added 
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
