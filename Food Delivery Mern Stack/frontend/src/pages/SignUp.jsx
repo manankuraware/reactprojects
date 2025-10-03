cd639441943c7e115e0ee64ec2b6bf5e93e3d07d
Manan Kuraware <manankuraware2017@gmail.com>
2025-10-03 22:47:28 +0530

    SignIn

--- Food Delivery Mern Stack/frontend/src/pages/SignUp.jsx
+++ Food Delivery Mern Stack/frontend/src/pages/SignUp.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa6";
 
 export const SignUp = () => {
   const bgColor = "#fff9f6";
   const borderColor = "#ddd";
-
+  const [showPassword, setShowPassword] = useState(false);
+  const [role, setRole] = useState("user");
   return (
     <div
       className="min-h-screen w-full flex items-center justify-center p-4"
@@ -54,6 +57,70 @@ export const SignUp = () => {
           />
         </div>
         {/* mobile number */}
+        <div className="mb-4">
+          <label
+            htmlFor="mobilenumber"
+            className="block text-gray-700 font-medium mb-1"
+          >
+            Number :
+          </label>
+          <input
+            id="mobilenumber"
+            type="number"
+            className="w-full border rounded-lg px-3 py-2 focus:outline-amber-500"
+            placeholder="Enter Your Phone Number"
+            style={{ border: `1px solid ${borderColor}` }}
+          />
+        </div>
+        {/* password */}
+        <div className="mb-4">
+          <label
+            htmlFor="password"
+            className="block text-gray-700 font-medium mb-1"
+          >
+            Password :
+          </label>
+          <div className="relative">
+            <input
+              id="password"
+              type={`${showPassword ? "text" : "password"}`}
+              className="w-full border rounded-lg px-3 py-2 focus:outline-amber-500"
+              placeholder="Enter Your Password"
+              style={{ border: `1px solid ${borderColor}` }}
+            />
+            <button
+              className="absolute right-3 cursor-pointer top-[15px] text-gray-500"
+              onClick={() => setShowPassword((prev) => !prev)}
+            >
+              {showPassword ? <FaEye /> : <FaEyeSlash />}
+            </button>
+          </div>
+        </div>
+
+        {/* role  */}
+        <div className="mb-4">
+          <label
+            htmlFor="role"
+            className="block text-gray-700 font-medium mb-1"
+          >
+            Role :
+          </label>
+          <div className="flex gap-2">
+            {["user", "owner", "delivery"].map((r) => (
+              <button
+                className="flex-1 border rounded-lg px-3 py-2 text-center font-medium transition-colors capitalize"
+                onClick={() => setRole(r)}
+                style={
+                  role == r
+                    ? { backgroundColor: "#e64323", color: "white" }
+                    : { borderColor: borderColor, color: "#333" }
+                }
+              >
+                {r}
+              </button>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
