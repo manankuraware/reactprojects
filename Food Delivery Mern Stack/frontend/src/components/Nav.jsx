67d02bd117410a204a535a4eda3039b5463471f5
Manan Kuraware <manankuraware2017@gmail.com>
2025-11-10 08:03:46 +0530

    location

--- Food Delivery Mern Stack/frontend/src/components/Nav.jsx
+++ Food Delivery Mern Stack/frontend/src/components/Nav.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaLocationDot } from "react-icons/fa6";
 import { IoIosSearch } from "react-icons/io";
 import { FiShoppingCart } from "react-icons/fi";
+import { RxCross2 } from "react-icons/rx";
 import { useSelector } from "react-redux";
 
 export default function Nav() {
   const { userData } = useSelector((state) => state.user);
+  const [showInfo, setShowInfo] = useState(false);
+  const [showSearch, setShowSearch] = useState(false);
   return (
     <div className="w-full h-[80px] flex items-center justify-between md:justify-center gap-[30px] px-[20px] fixed top-0 z-[9999] bg-[#fff9f6] overflow-visible">
+      {showSearch && (
+        <div className="w-[90%] h-[70px] bg-white shadow-xl rounded-lg items-center gap-[20px] flex fixed top-[80px] left-[5%]">
+          <div className="flex items-center w-[20%] overflow-hidden gap-[10px] px-[10px] border-r-[1px] border-gray-400">
+            <FaLocationDot size={25} className="text-[#ff4d2d]" />
+            <div className="w-[80%] truncate text-gray-600">Indore</div>
+          </div>
+          <div className="w-[80%] flex items-center gap-[10px]">
+            <IoIosSearch size={25} className="text-[#ff4d2d]" />
+            <input
+              type="text"
+              placeholder="Search delicious food..."
+              className="px-[10px] text-gray-700 outline-0 w-full"
+            />
+          </div>
+        </div>
+      )}
+
       <h1 className="text-3xl font-bold mb-2 text-[#ff4d2d]">Vingo</h1>
       <div className="md:w-[60%] lg:w-[40%] h-[70px] bg-white shadow-xl rounded-lg items-center gap-[20px] hidden md:flex">
         <div className="flex items-center w-[20%] overflow-hidden gap-[10px] px-[10px] border-r-[1px] border-gray-400">
@@ -15,7 +35,7 @@ export default function Nav() {
           <div className="w-[80%] truncate text-gray-600">Indore</div>
         </div>
         <div className="w-[80%] flex items-center gap-[10px]">
-          <IoIosSearch />
+          <IoIosSearch size={25} className="text-[#ff4d2d]" />
           <input
             type="text"
             placeholder="Search delicious food..."
@@ -25,6 +45,20 @@ export default function Nav() {
       </div>
 
       <div className="flex items-center gap-4">
+        {showSearch ? (
+          <RxCross2
+            size={25}
+            className="text-[#ff4d2d] md:hidden"
+            onClick={() => setShowSearch(false)}
+          />
+        ) : (
+          <IoIosSearch
+            size={25}
+            className="text-[#ff4d2d] md:hidden"
+            onClick={() => setShowSearch(true)}
+          />
+        )}
+
         <div className="relative cursor-pointer">
           <FiShoppingCart size={25} className="text-[#ff4d2d]" />
           <span className="absolute right-[-9px] top-[-12px] text-[#ff4d2d]">
@@ -34,9 +68,23 @@ export default function Nav() {
         <button className="hidden md:block px-3 py-1 rounded-lg bg-[#ff4d2d]/10 text-[#ff4d2d] text-sm font-medium">
           My Orders
         </button>
-        <div className="w-[40px] aspect-square rounded-full flex items-center justify-center bg-[#ff4d2d] text-white text-[18px] shadow-xl font-semibold cursor-pointer">
+        <div
+          className="w-[40px] aspect-square rounded-full flex items-center justify-center bg-[#ff4d2d] text-white text-[18px] shadow-xl font-semibold cursor-pointer"
+          onClick={() => setShowInfo((prev) => !prev)}
+        >
           {userData?.fullName.slice(0, 1)}
         </div>
+        {showInfo && (
+          <div className="fixed top-[80px] right-[10px] md:right-[10%] lg:right-[25%] w-[180px] bg-white shadow-2xl rounded-xl p-[20px] flex flex-col gap-[10px] z-[999]">
+            <div className="text-[17px] font-semibold">{userData.fullName}</div>
+            <div className="md:hidden text-[#ff4d2d] font-semibold cursor-pointer">
+              My Orders
+            </div>
+            <div className="text-[#ff4d2d] font-semibold cursor-pointer">
+              Log Out
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
---
+++ Food Delivery Mern Stack/frontend/src/hooks/useGetCity.jsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setCity } from "../redux/userSlice";
+
+export default function useGetCity() {
+  const dispatch = useDispatch();
+  const { userData } = useSelector((state) => state.user);
+  const apiKey = import.meta.env.VITE_GEOAPIKEY;
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(async (positon) => {
+      // console.log(positon);
+      const latitude = positon.coords.latitude;
+      const longitude = positon.coords.longitude;
+      const result = await axios.get(
+        `https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&format=json&apiKey=${apiKey}`
+      );
+      // console.log(result.data.results[0].city);
+      dispatch(setCity(result?.data?.results[0].city));
+    });
+  }, [userData]);
+}
