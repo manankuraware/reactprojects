99d64a9e5ca217cfbd216c91c78f56977fb7ea70
Manan Kuraware <manankuraware2017@gmail.com>
2026-01-04 21:09:27 +0530

    delete food items from owner dashboard

--- Food Delivery Mern Stack/backend/controllers/item.controllers.js
+++ Food Delivery Mern Stack/backend/controllers/item.controllers.js
@@ -79,3 +79,23 @@ export const getItemById = async (req, res) => {
     return res.status(500).json({ message: "GetItem Error" });
   }
 };
+
+export const deleteItem = async (req, res) => {
+  try {
+    const itemId = req.params.itemId;
+    const item = await Item.findByIdAndDelete(itemId);
+    if (!item) {
+      return res.status(400).json({ message: "Item not found" });
+    }
+    const shop = await Shop.findOne({ owner: req.userId });
+    shop.items = shop.items.filter((i) => i._id !== item._id);
+    await shop.save();
+    await shop.populate({
+      path: "items",
+      options: { sort: { updatedAt: -1 } },
+    });
+    return res.status(200).json(shop);
+  } catch (error) {
+    return res.status(500).json({ message: "Delete Item Error" });
+  }
+};
--- Food Delivery Mern Stack/frontend/src/components/OwnerItemCard.jsx
+++ Food Delivery Mern Stack/frontend/src/components/OwnerItemCard.jsx
@@ -1,10 +1,26 @@
+import axios from "axios";
 import React from "react";
 import { FaPen } from "react-icons/fa";
 import { FaTrashAlt } from "react-icons/fa";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router";
+import { serverUrl } from "../App";
+import { setMyShopData } from "../redux/ownerSlice";
 
 function OwnerItemcard({ data }) {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const handleDelete = async () => {
+    try {
+      const result = await axios.get(
+        `${serverUrl}/api/item/delete/${data._id}`,
+        { withCredentials: true }
+      );
+      dispatch(setMyShopData(result.data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="flex items-center bg-white rounded-lg shadow-md overflow-hidden border border-[#ff4d2d] w-full max-w-2xl p-1">
       <div className="w-36 h-full flex-shrink-0 bg-gray-50">
@@ -39,7 +55,10 @@ function OwnerItemcard({ data }) {
               <FaPen size={16} />
             </div>
 
-            <div className="p-2 rounded-full hover:bg-[#ff4d2d]/10 text-[#ff4d2d] cursor-pointer">
+            <div
+              className="p-2 rounded-full hover:bg-[#ff4d2d]/10 text-[#ff4d2d] cursor-pointer"
+              onClick={handleDelete}
+            >
               <FaTrashAlt size={16} />
             </div>
           </div>
