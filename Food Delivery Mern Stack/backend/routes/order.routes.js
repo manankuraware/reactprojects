36d4a2631fd0bbfbdde16da95f7b65153676c7c5
Manan Kuraware <manankuraware2017@gmail.com>
2026-02-17 08:03:01 +0530

    delivery boy order getorder updated

--- Food Delivery Mern Stack/backend/routes/order.routes.js
+++ Food Delivery Mern Stack/backend/routes/order.routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import isAuth from "../middlewares/isAuth.js";
 import {
   acceptOrder,
+  getCurrentOrder,
   getDeliveryBoyAssignment,
   getMyOrders,
   placeOrder,
@@ -15,5 +16,6 @@ orderRouter.get("/my-orders", isAuth, getMyOrders);
 orderRouter.post("/update-status/:orderId/:shopId", isAuth, updateOrderStatus);
 orderRouter.get("/get-assignments", isAuth, getDeliveryBoyAssignment);
 orderRouter.get("/accept-order/:assignmentId", isAuth, acceptOrder);
+orderRouter.get("/get-current-order", isAuth, getCurrentOrder);
 
 export default orderRouter;
--- Food Delivery Mern Stack/frontend/src/components/DeliveryBoy.jsx
+++ Food Delivery Mern Stack/frontend/src/components/DeliveryBoy.jsx
@@ -3,9 +3,11 @@ import Nav from "./Nav";
 import { useSelector } from "react-redux";
 import axios from "axios";
 import { serverUrl } from "../App";
+import DeliveryBoyTracking from "./DeliveryBoyTracking";
 
 function DeliveryBoy() {
   const { userData } = useSelector((state) => state.user);
+  const [currentOrder, setCurrentorder] = useState();
   const [availableAssignments, setAvailableAssignments] = useState(null);
   const getAssignments = async () => {
     try {
@@ -18,6 +20,18 @@ function DeliveryBoy() {
     }
   };
 
+  const getCurrentOrder = async () => {
+    try {
+      const result = await axios.get(
+        `${serverUrl}/api/order/get-current-order`,
+        { withCredentials: true },
+      );
+      setCurrentorder(result.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const acceptOrder = async (assignmentId) => {
     try {
       const result = await axios.get(
@@ -25,12 +39,14 @@ function DeliveryBoy() {
         { withCredentials: true },
       );
       console.log(result.data);
+      await getCurrentOrder();
     } catch (error) {
       console.log(error);
     }
   };
   useEffect(() => {
     getAssignments();
+    getCurrentOrder();
   }, [userData]);
   return (
     <div className="w-screen min-h-screen flex flex-col gap-5 items-center bg-[#fff9f6] overflow-y-auto">
@@ -49,41 +65,67 @@ function DeliveryBoy() {
           </p>
         </div>
 
-        <div className="bg-white rounded-2xl p-5 shadow-md w-[90%] border border-orange-100">
-          <h1 className="text-lg font-bold mb-4 flex items-center gap-2">
-            Available Orders
-          </h1>
+        {!currentOrder && (
+          <div className="bg-white rounded-2xl p-5 shadow-md w-[90%] border border-orange-100">
+            <h1 className="text-lg font-bold mb-4 flex items-center gap-2">
+              Available Orders
+            </h1>
 
-          <div className="space-y-4">
-            {availableAssignments?.length > 0 ? (
-              availableAssignments.map((a, index) => (
-                <div
-                  key={index}
-                  className="border rounded-lg p-4 flex justify-between items-center"
-                >
-                  <div>
-                    <p className="text-sm font-semibold">{a?.shopName}</p>
-                    <p className="text-sm text-gray-500">
-                      <span className="font-semibold">Delivery Address : </span>
-                      {a?.deliveryAddress?.text}
-                    </p>
-                    <p className="text-xs text-gray-400">
-                      {a?.items?.length} items | ₹{a?.subtotal}
-                    </p>
-                  </div>
-                  <button
-                    className="bg-orange-500 text-white px-4 py-1 rounded-lg text-sm hover:bg-orange-600"
-                    onClick={() => acceptOrder(a.assignmentId)}
+            <div className="space-y-4">
+              {availableAssignments?.length > 0 ? (
+                availableAssignments.map((a, index) => (
+                  <div
+                    key={index}
+                    className="border rounded-lg p-4 flex justify-between items-center"
                   >
-                    Accept
-                  </button>
-                </div>
-              ))
-            ) : (
-              <p className="text-gray-400 text-sm">No Available Orders</p>
-            )}
+                    <div>
+                      <p className="text-sm font-semibold">{a?.shopName}</p>
+                      <p className="text-sm text-gray-500">
+                        <span className="font-semibold">
+                          Delivery Address :{" "}
+                        </span>
+                        {a?.deliveryAddress?.text}
+                      </p>
+                      <p className="text-xs text-gray-400">
+                        {a?.items?.length} items | ₹{a?.subtotal}
+                      </p>
+                    </div>
+                    <button
+                      className="bg-orange-500 text-white px-4 py-1 rounded-lg text-sm hover:bg-orange-600"
+                      onClick={() => acceptOrder(a.assignmentId)}
+                    >
+                      Accept
+                    </button>
+                  </div>
+                ))
+              ) : (
+                <p className="text-gray-400 text-sm">No Available Orders</p>
+              )}
+            </div>
           </div>
-        </div>
+        )}
+
+        {currentOrder && (
+          <div className="bg-white rounded-2xl p-5 shadow-md w-[90%] border border-orange-100">
+            <h2 className="text-lg font-bold mb-3">📦 Current Order</h2>
+
+            <div className="border rounded-lg p-4 mb-3">
+              <p className="font-semibold text-sm">
+                {currentOrder?.shopOrder.shop.name}
+              </p>
+
+              <p className="text-sm text-gray-500">
+                {currentOrder.deliveryAddress.text}
+              </p>
+
+              <p className="text-xs text-gray-400">
+                {currentOrder.shopOrder.shopOrderItems.length} items |{" "}
+                {currentOrder.shopOrder.subtotal}
+              </p>
+            </div>
+            <DeliveryBoyTracking data={currentOrder} />
+          </div>
+        )}
       </div>
     </div>
   );
